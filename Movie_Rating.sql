edec8ce025304cd3209825b57cc82b66ccc4aeca
qichen90 <chittychen90@gmail.com>
2015-09-13 22:57:50 -0400

    Update Movie_Rating.sql

--- Movie_Rating.sql
+++ Movie_Rating.sql
@@ -286,3 +286,21 @@ from movie
 inner join rating using(mID)
 where director is not null
 group by director;
+
+
+/***************************** SQL Movie-Rating Modification Exercises *********************************/
+/*******************************************************************************************************/
+
+-- Q1 Add the reviewer Roger Ebert to your database, with an rID of 209.
+insert into reviewer values(209, 'Roger Ebert);
+
+-- Q2 Insert 5-star ratings by James Cameron for all movies in the database. Leave the review date as NULL.
+insert into rating values(207, 101, 5, null);
+insert into rating values(207, 101, 5, null);
+insert into rating values(207, 101, 5, null);
+insert into rating values(207, 101, 5, null);
+insert into rating values(207, 101, 5, null);
+
+-- Q3 For all movies that have an average rating of 4 stars or higher, add 25 to the release year. (Update the existing tuples; don't insert new tuples.)
+
+-- Q4 Remove all ratings where the movie's year is before 1970 or after 2000, and the rating is fewer than 4 stars.
